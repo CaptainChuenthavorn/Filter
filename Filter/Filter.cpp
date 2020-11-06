6913397ed429083681212d653d17ed57ddbd5642
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-11-06 10:54:07 +0700

    finish logic 2

--- Filter/Filter.cpp
+++ Filter/Filter.cpp
@@ -1,48 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+int  filter(int *p,int temp)
+{
+	int mod;
+	mod = temp % 10;
+	if (mod == 0) { *p +=1; }
+	else if (mod == 1) { *(p + 1) += 1; }
+	else if (mod == 2) { *(p + 2) += 1; }
+	else if (mod == 3) { *(p + 3) += 1; }
+	else if (mod == 4) { *(p + 4) += 1; }
+	else if (mod == 5) { *(p + 5) += 1; }
+	else if (mod == 6) { *(p + 6) += 1; }
+	else if (mod == 7) { *(p + 7) += 1; }
+	else if (mod == 8) { *(p + 8) += 1; }
+	else if (mod == 9) { *(p + 9) += 1; }
+	temp /= 10;
+	return temp;
+}
+
 int main()
 {
-	int n1,n2, i, a[10],mod,temp;
+	int n1, n2, i, a[10], mod, temp;
+	
 	for (i = 0;i < 10;i++)
 	{
 		a[i] = 0;
 		printf(" %d %d\n", i + 1, a[i]);
 	}
+	int* p = a;
 jumper:
 
-	printf("n1 > n2 && n1 < 0!!!\n");
+	printf("\nn1 > n2 && n1 < 0!!!\n");
 	printf("Eneter n1 :");
 	scanf("%d", &n1);
 	printf("Eneter n2 :");
 	scanf("%d", &n2);
-	if (n2 < n1 || n1<0)
+	if (n2 < n1 || n1 < 0)
 	{
 		goto jumper;
 	}
 	for (int j = n1;j <= n2;j++)
 	{
-		i = j;
-		temp = i;
-		while(temp!=0){
 		
-		
-		mod = temp % 10;
-		if (mod == 0) { a[0]++; }
-		else if (mod == 0) { a[0]++; }
-		else if (mod == 1) { a[1]++; }
-		else if (mod == 2) { a[2]++; }
-		else if (mod == 3) { a[3]++; }
-		else if (mod == 4) { a[4]++; }
-		else if (mod == 5) { a[5]++; }
-		else if (mod == 6) { a[6]++; }
-		else if (mod == 7) { a[7]++; }
-		else if (mod == 8) { a[8]++; }
-		else if (mod == 9) { a[9]++; }
-		temp /= 10;
+		temp = j;
+		while (temp != 0) {
+			temp = filter(p, temp);
 		}
-		i++;
+		
 	}
-	for(i=0;i<10;i++)
+	for (i = 0;i < 10;i++)
 
 		printf(" %d %d\n", i + 1, a[i]);
 	return 0;
